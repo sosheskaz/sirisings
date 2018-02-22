d5c0a0180a0451d33a17ed4d84b36e962482ae6f
Eric Miller <eric.the.miller@icloud.com>
2018-02-21 21:09:07 -0600

    added crappy saving functionality

--- sirisings/sirisings.py
+++ sirisings/sirisings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2 -W ignore
 import argparse
+import os
 import subprocess
 import urllib2
 from PyLyrics import PyLyrics
@@ -10,11 +11,12 @@ def main():
     parser = argparse.ArgumentParser(description='this app is kinda crappy, just a heads up')
     parser.add_argument('song', help='search terms for to find the song', nargs='+')
     parser.add_argument('-v', '--voice', help='voice for siri to sing with, default is samantha', default='samantha')
+    parser.add_argument('-s', '--save', help='instead of singing it, save it to the specified file so you can enjoy it later')
     args = parser.parse_args()
     candidates = search_for_song(' '.join(args.song))
     for singer, song in candidates:
         try:
-            sing_song(singer, song, args.voice)
+            sing_song(singer, song, args.voice, args.save)
             return
         except ValueError:
             continue
@@ -30,10 +32,17 @@ def search_for_song(search_terms):
     return list(pairs)
 
 
-def sing_song(singer, song, voice):
+def sing_song(singer, song, voice, savefile=None):
     lyrics = PyLyrics.getLyrics(singer, song)
     print urllib2.unquote('singing {} by {}'.format(song, singer).replace('_', ' '))
-    subprocess.call(['say', '-v', voice, lyrics])
+    saveargs = []
+    if savefile is not None:
+        basename, ext = os.path.splitext(savefile)
+        if ext.lower() != '.aiff':
+            savefile += '.aiff'
+        saveargs = ['-o', savefile]
+        print 'Saving to {}'.format(savefile)
+    subprocess.call(['say', '-v', voice] + saveargs + [voice, lyrics])
 
 
 if __name__ == '__main__':
